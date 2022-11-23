dc69c92f7f0bc4d610ed687c6c1524ad529883f6
matheuskassagas <gigliotti_@hotmail.com>
2022-11-23 14:05:35 -0300

    entities Room and video created

--- src/entities/Room.ts
+++ src/entities/Room.ts
@@ -1,7 +1,10 @@
-import { Entity } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('rooms')
 export class Room{
+  @PrimaryGeneratedColumn()
   id: number
+
+  @Column({ type: 'text' })
   name: string
 }
