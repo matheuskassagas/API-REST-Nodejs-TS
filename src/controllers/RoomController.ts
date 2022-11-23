c70718819a6046dfe8d1a26fad2d0b49a4012600
matheuskassagas <gigliotti_@hotmail.com>
2022-11-23 20:58:07 -0300

    method get room

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from "express";
+import { ObjectID } from "typeorm";
 import { RoomRepository } from "../repositories/RoomRepository";
+import { VideoRepository } from "../repositories/VideoRepository";
 
 export class RoomController {
+
+  // # METHOD CREATE # | endPoint /room .json to create an object room 
   async create(req: Request, res: Response){ // EndPoint Create
     const {name, description} = req.body
 
@@ -16,5 +20,39 @@ export class RoomController {
       return res.status(500).json({message: 'Internal Server Error'})
     }
     //return res.json('controller criado')
+  }
+
+  // # METHOD CREATE # | endPoint /room/id .json to create an object video in a room 
+  async createVideo(req: Request, res: Response){ // cria um video relacionando com uma room 
+    const {title, url} = req.body
+    const { idRoom } = req.params
+    
+    try { 
+      const room = await RoomRepository.findOneBy({id: Number(idRoom)}) //faz uma busca se ja existe a room
+      if(!room){
+        return res.status(404).json({message: 'Id nao encontrado'})
+      }
+      const newVideo = VideoRepository.create({ //caso nao existe ele cria uma nova
+        title, 
+        url, 
+        room
+      })
+
+      await VideoRepository.save(newVideo) // salva 
+      return res.status(201).json(newVideo) // retorn status e objeto criado
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({message: 'Internal Server Error'})
+    }
+  }
+  // # METHOD GET # | endPoint /room/id .json to get an object room 
+  async getRoom(req: Request, res: Response){
+    const { id } = req.params
+    const room = await RoomRepository.findOneBy({id: Number(id)})
+
+    if (!id){
+      return res.status(404).json({message: 'Id nao encontrado'})
+    }
+    return res.status(201).json(room)
   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { RoomController } from "./controllers/RoomController";
 import { SubjectController } from "./controllers/SubjectController";
-import { VideoController } from "./controllers/VideoController";
+
 
 
 const routes = Router()
 
 routes.post('/subject', new SubjectController().create)
 routes.post('/room', new RoomController().create)
-routes.post('/video', new VideoController().create)
+routes.post('/room/:idRoom', new RoomController().createVideo)
+routes.get('/room/:id', new RoomController().getRoom)
+
 
 export default routes
