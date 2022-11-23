95ff24da8f3fd5ace3414a1bb23ee8e16592ceed
matheuskassagas <gigliotti_@hotmail.com>
2022-11-23 10:13:51 -0300

    data base created

--- scr/data-source.ts
+++ scr/data-source.ts
@@ -1,5 +1,13 @@
 import { DataSource } from "typeorm";
 
-const AppDataSource = new DataSource ({
+const port = process.env.DB_PORT as number | undefined
+
+export const AppDataSource = new DataSource ({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: port,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   
 })
